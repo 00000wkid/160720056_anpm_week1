97e184d75ff7feac9aff40a95c257d8d2062e60c
00000wkid <xtrac8996@gmail.com>
2023-08-28 14:27:54 +0700

    modif code di login activity

--- app/src/main/java/com/example/testing/LoginActivity.kt
+++ app/src/main/java/com/example/testing/LoginActivity.kt
@@ -2,10 +2,17 @@ package com.example.testing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        var button=findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            Toast.makeText(this,"Login",Toast.LENGTH_LONG)
+        }
+
     }
 }
